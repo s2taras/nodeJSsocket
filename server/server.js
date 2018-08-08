07b9a0a65d58e44a156f346343a7517fe2d1b11c
Taras Litvin <litvint@i.ua>
2018-08-08 22:18:59 +0300

    separate socket, create model

--- server/server.js
+++ server/server.js
@@ -5,6 +5,10 @@ const app = express();
 const nunjucks = require('nunjucks');
 const server = require('http').Server(app);
 const io = require('socket.io')(server, {serveClient: true});
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/jschat', {useMongoClient: true});
+mongoose.Promise = require('bluebird');
 
 nunjucks.configure('../client/views', {
     autoescape: true,
@@ -17,23 +21,7 @@ app.get('/', (req, res) => {
 	res.render('index.html', {date: new Date()});
 });
 
-io.on('connection', function(socket) {
-	socket.emit('connected', 'You are connected!');
-
-	socket.join('all');
-
-	socket.on('msg', content => {
-		console.log(content);
-		const obj = {
-			date: new Date(),
-			content: content,
-			username: socket.id
-		};
-
-		socket.emit('message', obj);
-		socket.to('all').emit('message', obj);
-	});
-});
+require('./sockets')(io);
 
 server.listen(3000, () => {
 	console.log('Server started');
---
+++ server/sockets.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const MessageModel = require('./models/messages.model');
+
+module.exports = io => {
+	io.on('connection', function(socket) {
+		socket.emit('connected', 'You are connected!');
+
+		socket.join('all');
+
+		socket.on('msg', content => {
+			const obj = {
+				date: new Date(),
+				content: content,
+				username: socket.id
+			};
+
+			MessageModel.create(obj, err => {
+				if (err) return console.error('MessageModel', err);
+
+				socket.emit('message', obj);
+				socket.to('all').emit('message', obj);
+			});
+
+		});
+	});
+}
